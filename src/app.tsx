41698d40a69245fe491617540df4fa831381c682
akhilavipin <akhila.vipin22@gmail.com>
2025-07-04 10:05:48 +0200

    Navbar Implemented wiht menu

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,21 @@
-import APIcon from "./components/apicon";
+import APIcon from "./components/APIcon";
 import Intro from "./components/Intro";
-import ParticleBackground from "./components/particle-animation";
+import Navbar from "./components/Navbar";
+import ParticleBackground from "./components/Particle-animation";
 
 const App = () => {
   return (
     <div className="relative min-h-screen bg-black overflow-hidden">
-      <div className="absolute inset-0 z-0">
+      {/* <div className="absolute inset-0 z-0">
         <ParticleBackground />
-      </div>
-
+      </div> */}
+      {/* <div className="mb-8 transition duration-300 text-white hover:text-oceanSky">
+            <APIcon size={60} color="#ffffff" />
+          </div> */}
+          <Navbar />
       <main className="relative z-10 flex h-screen ">
         <section className="w-screen min-h-screen flex flex-col  px-6 py-10 text-white text-center">
-          <div className="mb-8 transition duration-300 text-white hover:text-oceanSky">
-            <APIcon size={60} color="#ffffff" />
-          </div>
-          <Intro />
+          
         </section>
       </main>
     </div>

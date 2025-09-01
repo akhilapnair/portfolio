36ea4e5d3902dcc47375420d23249d60bad82980
akhilavipin <akhila.vipin22@gmail.com>
2025-09-01 22:30:07 +0200

    experience timeline logo added and css changes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,16 +3,72 @@ import type { ExperienceCard } from "../types/Experience-card";
 const expCards: ExperienceCard[] = [
   {
     companyBrief:
-      "GattyInstruments AB is a Swedish R&D company in Uppsala specializing in mobile respiratory analyzers for healthcare professionals and respiratory health users.",
+      "GattyInstruments AB ",
     imgPath: "/images/exp2.png",
-    logoPath: "/images/logo2.png",
+    logoPath: "/images/gattyI_logo.jpg",
     title: "Full Stack Developer",
     date: "June 2020 - December 2023",
     responsibilities: [
-      
       "The company required a secure, user-friendly doctor’s dashboard to manage clinical data efficiently",
-      "I developed and integrated core features for the doctor's dashboard using React.js, Tailwind CSS, and TypeScript for a modern, responsive UI. ",
-      "I built scalable APIs and managed data using Node.js and MySQL. To ensure reliability, I wrote unit tests with Jest, contributing to a production-ready system that supported the product’s timely certification and launch.",
+      "developed and integrated core features for the doctor's dashboard using React.js, Tailwind CSS, and TypeScript for a modern, responsive UI. ",
+      "built scalable APIs and managed data using Node.js and MySQL.",
+      " To ensure reliability, I wrote unit tests with Jest, contributing to a production-ready system that supported the product’s timely certification and launch.",
+    ],
+  },
+    {
+    companyBrief:
+      "IBS Software Services Pvt Ltd ",
+    imgPath: "/images/.jpg",
+    logoPath: "/images/IBS_Logo.jpg",
+    title: "Software Developer",
+    date: "June 2020 - December 2023",
+    responsibilities: [
+      "The company required a secure, user-friendly doctor’s dashboard to manage clinical data efficiently",
+      "developed and integrated core features for the doctor's dashboard using React.js, Tailwind CSS, and TypeScript for a modern, responsive UI. ",
+      "built scalable APIs and managed data using Node.js and MySQL.",
+      " To ensure reliability, I wrote unit tests with Jest, contributing to a production-ready system that supported the product’s timely certification and launch.",
+    ],
+  },
+  {
+    companyBrief:
+      "Kameda Infologics Pvt Ltd ",
+    imgPath: "/images/exp2.png",
+    logoPath: "/images/kamedainfologics_logo.jpg",
+    title: "Software Developer",
+    date: "June 2020 - December 2023",
+    responsibilities: [
+      "The company required a secure, user-friendly doctor’s dashboard to manage clinical data efficiently",
+      "developed and integrated core features for the doctor's dashboard using React.js, Tailwind CSS, and TypeScript for a modern, responsive UI. ",
+      "built scalable APIs and managed data using Node.js and MySQL.",
+      " To ensure reliability, I wrote unit tests with Jest, contributing to a production-ready system that supported the product’s timely certification and launch.",
+    ],
+  },
+  {
+    companyBrief:
+      "Ust Global Pvt Ltd ",
+    imgPath: "/images/exp2.png",
+    logoPath: "/images/ust_logo.svg",
+    title: "Software Developer",
+    date: "June 2020 - December 2023",
+    responsibilities: [
+      "The company required a secure, user-friendly doctor’s dashboard to manage clinical data efficiently",
+      "developed and integrated core features for the doctor's dashboard using React.js, Tailwind CSS, and TypeScript for a modern, responsive UI. ",
+      "built scalable APIs and managed data using Node.js and MySQL.",
+      " To ensure reliability, I wrote unit tests with Jest, contributing to a production-ready system that supported the product’s timely certification and launch.",
+    ],
+  },
+    {
+    companyBrief:
+      "Neoito Technologies Pvt Ltd ",
+    imgPath: "/images/exp2.png",
+    logoPath: "/images/neoito-logo.png",
+    title: "Software Developer",
+    date: "June 2020 - December 2023",
+    responsibilities: [
+      "The company required a secure, user-friendly doctor’s dashboard to manage clinical data efficiently",
+      "developed and integrated core features for the doctor's dashboard using React.js, Tailwind CSS, and TypeScript for a modern, responsive UI. ",
+      "built scalable APIs and managed data using Node.js and MySQL.",
+      " To ensure reliability, I wrote unit tests with Jest, contributing to a production-ready system that supported the product’s timely certification and launch.",
     ],
   },
 ];
--- src/section/Experience.tsx
+++ src/section/Experience.tsx
@@ -1,10 +1,55 @@
-import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import TitleHeader from "../components/TitleHeader";
 import { expCards } from "../constants";
 import GlowCard from "../components/GlowCard";
 import type { ExperienceCard } from "../types/Experience-card";
 
 const Experience = () => {
+  useGSAP(() => {
+    gsap.utils.toArray(".timeline-card").forEach((card: any) => {
+      gsap.from(card, {
+        xPercent: -100,
+        opacity: 0,
+        transformOrigin: "left left",
+        duration: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: card,
+          start: "top 80%",
+        },
+      });
+    });
+    gsap.to(".timeline", {
+      transformOrigin: "bottom bottom",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: ".timeline",
+        start: "top center",
+        end: "70% center",
+        onUpdate: (self) => {
+          gsap.to(".timeline", {
+            scaleY: 1 - self.progress,
+          });
+        },
+      },
+    });
+
+    gsap.utils.toArray(".expText").forEach((text: any) => {
+      gsap.from(text, {
+        opacity: 0,
+        xPercent: 0,
+        duration: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: text,
+          start: "top 60%",
+        },
+      });
+    }, "<");
+  }, []);
   return (
     <section
       id="experience"
@@ -16,33 +61,38 @@ const Experience = () => {
           sub="💼 My Career Overview"
         />
         <div className="mt-32 relative">
-             <div className="mt-32 relative">
-          <div className="relative z-50 xl:space-y-32 space-y-10">
-            {expCards.map((card:ExperienceCard,index:number) => (
-              <div key={card.title} className="exp-card-wrapper">
-                <div className="xl:w-2/6">
-                  <GlowCard card={card} index={index}>
-                    <div>
+          <div className="mt-32 relative">
+            <div className="relative z-50 xl:space-y-32 space-y-10">
+              {expCards.map((card: ExperienceCard, index: number) => (
+                <div key={card.title} className="exp-card-wrapper">
+                  <div className="xl:w-2/6">
+                    <p className="text-white-50 text-lg">{card.companyBrief}</p>
+                    {/* <div>
                       <img src={card.imgPath} alt="exp-img" />
-                    </div>
-                  </GlowCard>
-                </div>
-                <div className="xl:w-4/6">
-                  <div className="flex items-start">
-                    <div className="timeline-wrapper">
-                      <div className="timeline" />
-                      <div className="gradient-line w-1 h-full" />
-                    </div>
-                    <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
-                      <div className="timeline-logo">
-                        <img src={card.logoPath} alt="logo" />
+                    </div> */}
+                  </div>
+                  <div className="xl:w-4/6">
+                    <div className="flex items-start">
+                      <div className="timeline-wrapper">
+                        <div className="timeline" />
+                        <div className="gradient-line w-1 h-full" />
                       </div>
-                      <div>
-                        <h1 className="font-semibold text-3xl">{card.title}</h1>
-                        <p className="my-5 text-white-50">
-                          🗓️&nbsp;{card.date}
-                        </p>
-                        <p className="text-[#839CB5] italic">
+                      <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                        <div className="timeline-logo">
+                          <img
+                            src={card.logoPath}
+                            className="w-full h-full object-cover"
+                            alt="logo"
+                          />
+                        </div>
+                        <div>
+                          <h1 className="font-semibold text-3xl">
+                            {card.title}
+                          </h1>
+                          <p className="my-5 text-white-50">
+                            🗓️&nbsp;{card.date}
+                          </p>
+                          {/* <p className="text-[#839CB5] italic">
                           Responsibilities
                         </p>
                         <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
@@ -53,15 +103,15 @@ const Experience = () => {
                               </li>
                             )
                           )}
-                        </ul>
+                        </ul> */}
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
           {/* <div className="relative z-50 xl:space-y-32 space-y-10">
             {expCards.map((card: any) => (
               <div key={card.title} className="exp-card-wrapper">
